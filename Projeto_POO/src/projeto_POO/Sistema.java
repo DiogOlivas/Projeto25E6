6cb4c6066e914aaffcff19daf3cb828f98b40d42
AntónioCarvalho <51620@alunos.upt.pt>
2025-06-10 15:23:10 +0100

    Update Sistema.java

--- Projeto_POO/src/projeto_POO/Sistema.java
+++ Projeto_POO/src/projeto_POO/Sistema.java
@@ -1,5 +1,19 @@
 package projeto_POO;
+import java.util.ArrayList;
 
 public class Sistema {
+
+	public static double calcularMediaLeituras(Sensor sensor) {
+		
+	ArrayList<Leitura> leituras = sensor.getLeituras();
 	
-}
+	if (leituras.isEmpty()) {
+		return 0;
+	}
+		double soma = 0;
+		for (Leitura a : leituras) {
+			soma += a.getValor();
+	}
+		return soma / leituras.size();
+	}
+	}
